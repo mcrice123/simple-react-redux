464f484fb17cd59765f845c4bed581c1fa4b2eca
Maria Rice <mcrice@shockers.wichita.edu>
2018-01-10 15:13:03 -0600

    added more info to container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+import UserInput from '../common/UserInput';
+
 class Container extends Component {
+	constructor() {
+		super();
+
+		this.state = { 
+			input: ''
+		};
+		this.inputDidChange = this.inputDidChange.bind(this);
+	}
+
+	inputDidChange(e) {
+		console.log(e.target.value);
+		this.setState({input: e.target.value});
+	}
+
 	render() {
 		return (
 			<div style={{marginTop: '10px', padding: '10px', border: '1px solid green', width: '50%'}}>
-				<label>This is a container ("smart component")</label>
+				<label style={{marginRight: '10px', display: 'block'}}>This is a <strong>container</strong> ("smart component")</label>
+				<label style={{marginRight: '10px'}}>Input from App State</label>
+				<UserInput />
+				<label style={{marginRight: '10px'}}>Input from Component State</label>
+				<UserInput value={this.state.input} onChange={this.inputDidChange} />
 			</div>
 		);
 	}
