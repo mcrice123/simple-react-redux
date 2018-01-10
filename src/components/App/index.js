626220d1d5b5bc1bd3f4d3686b30f511ac204855
Maria Rice <mcrice@shockers.wichita.edu>
2018-01-10 14:06:41 -0600

    added styling to app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -18,18 +18,17 @@ export default class App extends Component {
   render() {
     return (
       <div style={{marginLeft: '20px'}}>
-      	<h1 style={{marginTop: '30px'}}>My awesome app</h1>
+      	<h1 style={{marginTop: '30px'}}>My awesome app with Redux</h1>
 
-      	<button onClick={this.onClickAddOne.bind(this)} style={{marginBottom: '10px'}}>Add One</button>
+        <div id="app-component" style={{border: '1px solid red', padding: '10px', width: '50%'}}>
+      	  <button onClick={this.onClickAddOne.bind(this)} style={{marginBottom: '10px', marginTop: '50px'}}>Add One</button>
 
+          <UserInput placeholder="Input Box 1" value={this.state.value} onChange={(e) => this.setState({ value: e.target.value})} style={{display: 'block'}} />
 
-        <UserInput placeholder="Input Box 1" value={this.state.value} onChange={(e) => this.setState({ value: e.target.value})} style={{display: 'block'}} />
-
-
-        <h1 style={{marginTop: '200px'}}>What's in the state?</h1>
-        <h2>Current Count: <span style={{color: 'red'}}>{this.state.count}</span></h2>
-        <h2>Input Box 1: <span style={{color: 'red'}}>{this.state.value}</span></h2>
-
+          <h2 style={{marginTop: '50px'}}>What's in the App state?</h2>
+          <p>Current Count: <span style={{color: 'red'}}>{this.state.count}</span></p>
+          <p>Input Box 1: <span style={{color: 'red'}}>{this.state.value}</span></p>
+        </div>
 
 
       </div>
